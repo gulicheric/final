42218a9a37b52b6f0b9f816c0cd23e0698b2e0f8
gulicheric <54865893+gulicheric@users.noreply.github.com>
2021-05-11 01:34:08 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -35,8 +35,6 @@ def create_app():
 
     db.init_app(app)
     login_manager.init_app(app)
-    # login_manager.login_view = "login"
-
     bcrypt.init_app(app)
 
     from app.posts.routes import posts
