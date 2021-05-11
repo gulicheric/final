157f9b6de9af446949af75ba9d0f4e75de16a48e
gulicheric <54865893+gulicheric@users.noreply.github.com>
2021-05-11 02:01:17 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -31,7 +31,7 @@ def create_app():
         SESSION_COOKIE_HTTPONLY=True,
         SESSION_COOKIE_SAMESITE="Lax",
     )
-    app.config["MONGO_URI"] ="mongodb://localhost:27017/perspect_db"
+    app.config["MONGO_URI"] = os.getenv("MONGODB_HOST")
 
     db.init_app(app)
     login_manager.init_app(app)
